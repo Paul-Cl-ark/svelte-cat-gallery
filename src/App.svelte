175b17e3ba86021ecbed7ee75dc5562c4cfec2be
Paul Clark <clark.paul123@gmail.com>
2020-10-04 16:57:46 +0100

    refactor api call, get 6 cats and add basic size

--- src/App.svelte
+++ src/App.svelte
@@ -4,21 +4,27 @@
   let images = []
 
   onMount(() => {
-    const path = 'https://api.thecatapi.com/v1/images/search?limit=5'
+    const path = 'https://api.thecatapi.com/v1/images/search?limit=6'
     const apiKey = `&api_key=${process.env.CAT_API_KEY}`
     fetch(`${path}${apiKey}`)
       .then((response) => response.json())
-      .then((response) => {
-        console.log(response)
-        images = response
-      })
-      .catch((err) => [])
+      .then((response) => (images = response))
+      .catch((err) => {})
   })
 </script>
 
 <div>
-  <p>Hello World</p>
   {#each images as image}
   <img src="{image.url}" alt="A nice cat" />
   {/each}
 </div>
+
+<style>
+  img {
+    width: 300px;
+    height: 300px;
+
+    /*Scale down will take the necessary specified space that is 100px x 100px without stretching the image*/
+    object-fit: scale-down;
+  }
+</style>
