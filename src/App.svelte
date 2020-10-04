8727ef88fd93848879bdec9471d73aa20f81a135
Paul Clark <clark.paul123@gmail.com>
2020-10-04 16:46:08 +0100

    get some cat pics

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,24 @@
-<p>Hello World</p>
+<script>
+  import { onMount } from 'svelte'
+
+  let images = []
+
+  onMount(() => {
+    const path = 'https://api.thecatapi.com/v1/images/search?limit=5'
+    const apiKey = `&api_key=${process.env.CAT_API_KEY}`
+    fetch(`${path}${apiKey}`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response)
+        images = response
+      })
+      .catch((err) => [])
+  })
+</script>
+
+<div>
+  <p>Hello World</p>
+  {#each images as image}
+  <img src="{image.url}" alt="A nice cat" />
+  {/each}
+</div>
