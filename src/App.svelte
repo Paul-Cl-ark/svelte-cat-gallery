43c770cc95f7cec7ca5a1b2531349ec5b67feaed
Paul Clark <clark.paul123@gmail.com>
2020-10-16 22:07:31 +0100

    loading cat gif

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import Image from './components/Image'
 
   let images = []
+  let loading = true
 
   const hitCatApi = (limit = 6) => {
     const path = `https://api.thecatapi.com/v1/images/search?limit=${limit}`
@@ -12,7 +13,10 @@
       .catch((err) => [])
   }
 
-  onMount(async () => images = await hitCatApi())
+  onMount(async () => {
+    images = await hitCatApi()
+    loading = false
+  })
 
   const getNewCat = async ({detail}) => {
     const [newCat] = await hitCatApi(1)
@@ -22,8 +26,16 @@
   }
  </script>
 
-<div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
+{#if loading}
+  <div class="flex h-screen">
+    <img class="m-auto rounded-full" src="https://media1.tenor.com/images/2900121f5d66061f8a5d8d34c41d4454/tenor.gif" loop=infinite/>            
+  </div>
+{:else}
+  <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
+  {#if images.length}
   {#each images as image, index}
     <Image {image} {index} on:getNewCat={getNewCat} />
   {/each}
-</div>
+  {/if}
+  </div>
+{/if}
