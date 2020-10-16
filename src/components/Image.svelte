e5a1ef529298592a5f910a1c968ed6d845937d05
Paul Clark <clark.paul123@gmail.com>
2020-10-16 20:21:58 +0100

    add button

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -3,13 +3,23 @@
 </script>
 
 <div class="wrapper">
-  <div style={
+  <div
+    class="relative" 
+    style={
     `background: url(${image.url});
     background-size: cover;
     background-position: center;
     min-height: 100%;
     border-radius: 5px;`
-  }/>
+  }
+  >
+    <button class="
+      m-3 absolute right-0 bottom-0 bg-pink-400 hover:bg-pink-500 text-white 
+      font-bold py-2 px-4 rounded"
+    >
+      CHANGE DIS WAN!!
+    </button>
+  </div>
 </div>
 
 <style>
